294c87b75b1965b312fd982febf7a7dcb39e6edb
CalacalaBoom <zhangyu19990502@163.com>
2021-11-18 12:54:34 +0800

    在自定义类中定义全局变量和绘制贪吃蛇的方法

---
+++ GreedySnake/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace GreedySnake
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.设置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.控制ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.设置ToolStripMenuItem,
+            this.控制ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(526, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 设置ToolStripMenuItem
+            // 
+            this.设置ToolStripMenuItem.Name = "设置ToolStripMenuItem";
+            this.设置ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.设置ToolStripMenuItem.Text = "设置";
+            // 
+            // 控制ToolStripMenuItem
+            // 
+            this.控制ToolStripMenuItem.Name = "控制ToolStripMenuItem";
+            this.控制ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.控制ToolStripMenuItem.Text = "控制";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 75);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(502, 285);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(243, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(211, 52);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 372);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "贪吃蛇";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 设置ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 控制ToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ GreedySnake/Snake.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Collections;
+
+namespace GreedySnake
+{
+    class Snake
+    {
+        //定义全局变量
+        public static int Condyle = 0;  //设置骨节大小
+        public static int Aspect = 0;   //设置方向
+        public static Point[] Place = {new Point(-1,-1),new Point(-1,-1), new Point(-1, -1),
+            new Point(-1, -1), new Point(-1, -1), new Point(-1, -1),};  //设置各个骨节的位置
+        public static Point Food = new Point(-1,-1); //设置食物所在点
+        public static bool ifFood = false;  //是否有食物
+        public static bool ifGame = false;  //游戏是否结束
+        public static int Field_width = 0;  //场地的宽度
+        public static int Field_height = 0;  //场地的高度
+        public static Control control;  //记录绘制贪吃蛇的控件
+        public static Timer time;  //记录Timer组件
+        public static SolidBrush SolidB = new SolidBrush(Color.Red);  //设置贪吃蛇身体的颜色
+        public static SolidBrush SolidD = new SolidBrush(Color.LightCoral);  //设置背景颜色
+        public static SolidBrush SolidF = new SolidBrush(Color.Blue);  //设置食物颜色
+        public static Label label;  //记录Label控件
+        public static ArrayList List = new ArrayList();  //实例化ArrayList
+        Graphics g;  //实例化Graphics类
+
+        //自定义方法Ophidian用于绘制贪吃蛇的起始位置，以及对游戏进行初始化设置 
+        public void Ophidian(Control Con,int condyle)
+        {
+            Field_width = Con.Width;  //获取场地宽度
+            Field_height = Con.Height;  //获取场地高度
+            Condyle = condyle;  //记录骨节大小
+            control = Con;  //记录背景控件
+            g = control.CreateGraphics();  //创建背景控件的Graphics类
+            SolidD = new SolidBrush(Con.BackColor);  //设置画刷颜色
+            for (int i = 0; i < Place.Length; i++)  //绘制贪吃蛇
+            {
+                Place[i].X = (Place.Length - i - 1) * Condyle;  //设置骨节横坐标位置
+                Place[i].Y = (Field_height / 2) - Condyle;  //设置骨节纵坐标位置
+                //绘制骨节
+                g.FillRectangle(SolidB, Place[i].X + 1, Place[i].Y + 1, Condyle - 1, Condyle - 1);
+            }
+            List = new ArrayList(Place);
+            ifGame = false;
+            Aspect = 0;
+        }
+
+    }
+}
